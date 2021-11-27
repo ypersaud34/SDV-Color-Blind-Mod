2be6b817b2206f574dfe66a9085ab0a9dea98f69
Yojeraj Persaud <ypersaud34@gmail.com>
2021-11-27 14:44:18 -0500

    Added more code to replace vanilla assets with modified ones.

--- SDVColorBlindMod/ModEntry.cs
+++ SDVColorBlindMod/ModEntry.cs
@@ -27,7 +27,32 @@ namespace SDVColorBlindMod
             Texture2D sourceImage = this.Helper.Content.Load<Texture2D>("custom-texture.png", ContentSource.ModFolder);
             editor.PatchImage(sourceImage, targetArea: new Rectangle(300, 100, 200, 200));
         }
+        /// <summary>Get whether this instance can load the initial version of the given asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Portraits/Abigail"))
+            {
+                return true;
+            }
 
+            return false;
+        }
+
+        /// <summary>Load a matched asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public T Load<T>(IAssetInfo asset)
+        {
+            String assetName = asset.AssetName;
+
+            if (assetName.Equals("Portraits/Abigail"))
+            {
+
+                return this.Helper.Content.Load<T>("assets/abigail-portaits.png", ContentSource.ModFolder);
+            }
+
+            throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+        }
 
         /*********
         ** Private methods
