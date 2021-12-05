ce44b62e1d7c2ae001d341f2102499d7165f3bb8
Justin Huang <57599179+JustinHuang17@users.noreply.github.com>
2021-12-04 21:51:56 -0500

    Fixed if statements / Cleaned up Code
    Removed useless methods and left the two relevant ones that checks and loads the assests

--- SDVColorBlindMod/ModEntry.cs
+++ SDVColorBlindMod/ModEntry.cs
@@ -8,80 +8,56 @@ using StardewValley;
 
 namespace SDVColorBlindMod
 {
-    /// <summary>The mod entry point.</summary>
+ 
     public class ModEntry : Mod, IAssetLoader
     {
-        /*********
-        ** Public methods
-        *********/
-        
-        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
-        /// <param name="helper">Provides simplified APIs for writing mods.</param>
-        public override void Entry(IModHelper helper)
-        {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+   
 
-        }
-        public void Edit<T>(IAssetData asset)
-        {
-            var editor = asset.AsImage();
-
-            Texture2D sourceImage = this.Helper.Content.Load<Texture2D>("custom-texture.png", ContentSource.ModFolder);
-            editor.PatchImage(sourceImage, targetArea: new Rectangle(300, 100, 200, 200));
-        }
-        /// <summary>Get whether this instance can load the initial version of the given asset.</summary>
-        /// <param name="asset">Basic metadata about the asset being loaded.</param>
         public bool CanLoad<T>(IAssetInfo asset)
         {
             if (asset.AssetNameEquals("TileSheets/crops") ||
-               asset.AssetNameEquals("TileSheets/bushes") || 
-               asset.AssetNameEquals("Maps/springobjects")){
+                asset.AssetNameEquals("Maps/springobjects") ||
+                asset.AssetNameEquals("TileSheets/bushes") ||
+                asset.AssetNameEquals("Maps/paths")
+                )
+            {
 
                 return true;
             }
-                return false;
-            
-            }
+            return false;
+
         }
 
-        /// <summary>Load a matched asset.</summary>
-        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+
         public T Load<T>(IAssetInfo asset)
         {
 
-        if (asset.AssetNameEquals("TileSheets/crops"))
-        {
-            return this.Helper.Content.Load<T>("assets/crops.png", ContentSource.ModFolder);
-        }
-        else if (asset.AssetNameEquals("TileSheets/bushes"))
-        {
-            return this.Helper.Content.Load<T>("assets/bushes.png", ContentSource.ModFolder);
-        }
-        else if (asset.AssetNameEquals("Maps/springsobjects"))
-        {
-            return this.Helper.Content.Load<T>("assets/springobjects.png", ContentSource.ModFolder);
-        }
-               
-         throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
-
+            if (asset.AssetNameEquals("TileSheets/crops"))
+            {
+                return this.Helper.Content.Load<T>("assets/crops.png", ContentSource.ModFolder);
             }
+            if (asset.AssetNameEquals("TileSheets/bushes"))
+            {
+                return this.Helper.Content.Load<T>("assets/bushes.png", ContentSource.ModFolder);
+            }
+            if (asset.AssetNameEquals("Maps/springobjects"))
+            {
+                return this.Helper.Content.Load<T>("assets/springobjects.png", ContentSource.ModFolder);
+            }
+            if (asset.AssetNameEquals("Maps/paths"))
+            {
+                return this.Helper.Content.Load<T>("assets/paths.png", ContentSource.ModFolder);
+            }
+
+            throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+
         }
-
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-        }
-
     }
+
+
 }
+    
